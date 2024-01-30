7bdb42c738337f9da35d02bb599ecf00221b8dc4
Alex <svds22090@gmail.com>
2024-01-30 19:10:24 +0300

    upd

--- outline_api/outline_api.py
+++ outline_api/outline_api.py
@@ -2,32 +2,22 @@ import os
 
 
 class OutlineServer:
-    server_keys = set()
 
     def __init__(self, outline_api_url):
-        self.__outline_api_url = outline_api_url
-        self.server_keys.add(outline_api_url)
-
-    @classmethod
-    def check_unique_key(cls, arg):
-        return arg in cls.server_keys
+        self._outline_api_url = outline_api_url
 
     @property
-    def key(self):
-        return self.__outline_api_url
+    def server_key(self):
+        return self._outline_api_url
 
-    @key.setter
-    def key(self, outline_api_url):
-        self.__outline_api_url = outline_api_url
+    @server_key.setter
+    def server_key(self, outline_api_url):
+        self._outline_api_url = outline_api_url
 
-    @key.deleter
-    def key(self):
-        self.server_keys.remove(self.__outline_api_url)
-        del self.__outline_api_url
+    @server_key.deleter
+    def server_key(self):
+        del self._outline_api_url
 
 
 test = OutlineServer(123)
-print(test.key)
-
 test2 = OutlineServer(456)
-print(test2.key)
