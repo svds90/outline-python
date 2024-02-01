855c441dc4f88390569277fa64d5232e020e5281
Alex <svds22090@gmail.com>
2024-02-01 18:55:04 +0300

    upd

--- outline_api/outline_api.py
+++ outline_api/outline_api.py
@@ -9,15 +9,27 @@ class OutlineServer:
     server_keys = set()
 
     def __init__(self, outline_api_url: str):
-        self._outline_api_url = self.__handle_server_key(
-            outline_api_url, from_init=True)
+        self._outline_api_url = self.__handle_server_key(outline_api_url)
+
+    def __handle_server_key(self, outline_api_url: str) -> Optional[str]:
+
+        if self.__check_server_key(outline_api_url):
+            raise InvalidServerKeyError(
+                f"Key {outline_api_url} already exists")
+
+        elif self.__validate_server_key(outline_api_url):
+            try:
+                self.__remove_server_key()
+            finally:
+                self.__add_server_key(outline_api_url)
+                return outline_api_url
 
     def __validate_server_key(self, outline_api_url: str) -> bool:
         if not isinstance(outline_api_url, str):
-            print("Key must be a string")
+            raise InvalidServerKeyError("Key must be a string")
             return False
         elif len(outline_api_url) == 0:
-            print("Empty value")
+            raise InvalidServerKeyError("Empty value")
             return False
         else:
             return True
@@ -31,24 +43,6 @@ class OutlineServer:
     def __remove_server_key(self):
         if self._outline_api_url in self.server_keys:
             self.server_keys.remove(self._outline_api_url)
-
-    def __handle_server_key(self, outline_api_url: str,
-                            from_init: bool = False) -> Optional[str]:
-
-        if self.__check_server_key(outline_api_url):
-            print(f"Key {outline_api_url} already exists!")
-        elif self.__validate_server_key(outline_api_url):
-            if from_init:
-                print("CALL FROM INIT")
-                self.__add_server_key(outline_api_url)
-                return outline_api_url
-            elif not from_init:
-                print("CALL FROM SETTER")
-                self.__remove_server_key()
-                self.__add_server_key(outline_api_url)
-                return outline_api_url
-        else:
-            raise InvalidServerKeyError("Invalid Outline API URL")
 
     @property
     def server_key(self) -> Optional[str]:
