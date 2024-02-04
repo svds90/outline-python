fed808ad410dc64f52c0c607bdbc749efadf5608
Alex <svds22090@gmail.com>
2024-02-04 18:59:11 +0300

    upd

--- outline_api/outline_api.py
+++ outline_api/outline_api.py
@@ -1,8 +1,41 @@
-from typing import Optional
+from typing import Any, Optional
 
 
-class InvalidServerKeyError(TypeError):
+class InvalidServerKeyError(Exception):
     pass
+
+
+class OutlineClients:
+    __instance = None
+    clients = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self, server_name, outline_api_url):
+        self._outline_api_url = self.__handle_server_key(outline_api_url)
+        self.clients[server_name] = outline_api_url
+
+    def __getattr__(self, server_name):
+        if server_name in self.clients:
+            return self.clients[server_name]
+        else:
+            print("Server not found")
+
+    def set_server_url(self, server_name, outline_api_url):
+        if server_name in self.clients:
+            self.clients[server_name] = outline_api_url
+        else:
+            print("Server not found")
+
+    def __del__(self):
+        OutlineClients.__instance = None
+
+    def __handle_server_key(self, outline_api_url: str) -> Optional[str]:
+        """Adds unique keys, removes old ones if reassigned"""
+        return outline_api_url
 
 
 class OutlineServer:
@@ -12,12 +45,9 @@ class OutlineServer:
         self._outline_api_url = self.__handle_server_key(outline_api_url)
 
     def __handle_server_key(self, outline_api_url: str) -> Optional[str]:
+        """Adds unique keys, removes old ones if reassigned"""
 
-        if self.__check_server_key(outline_api_url):
-            raise InvalidServerKeyError(
-                f"Key {outline_api_url} already exists")
-
-        elif self.__validate_server_key(outline_api_url):
+        if self.__validate_server_key(outline_api_url):
             try:
                 self.__remove_server_key()
             finally:
@@ -25,11 +55,18 @@ class OutlineServer:
                 return outline_api_url
 
     def __validate_server_key(self, outline_api_url: str) -> bool:
+        """Validates whether the server key is acceptable"""
 
-        if not isinstance(outline_api_url, str):
+        if self.__check_server_key(outline_api_url):
+            raise InvalidServerKeyError(
+                f"Key {outline_api_url} already exists")
+
+        elif not isinstance(outline_api_url, str):
             raise InvalidServerKeyError("Key must be a string")
+
         elif len(outline_api_url.strip()) == 0:
             raise InvalidServerKeyError("Empty value")
+
         else:
             return True
 
@@ -57,5 +94,5 @@ class OutlineServer:
         del self._outline_api_url
 
 
-test = OutlineServer("123")
-test2 = OutlineServer("456")
+# test = OutlineServer("123")
+# test2 = OutlineServer("456")
